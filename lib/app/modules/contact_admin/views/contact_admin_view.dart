6ba0fa1242d027bba8849649389f81b505460857
Habib Nurrohmad Sugiharto <habib091103@gmail.com>
2025-03-21 11:08:08 +0700

    Update contact_admin_view.dart

--- lib/app/modules/contact_admin/views/contact_admin_view.dart
+++ lib/app/modules/contact_admin/views/contact_admin_view.dart
@@ -2,108 +2,147 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mob_dev_wave_coach/app/core/values/app_colors.dart';
-
 import '../controllers/contact_admin_controller.dart';
 
-class ContactAdminView extends GetView<ContactAdminController> {
+class ContactAdminView extends StatefulWidget {
   const ContactAdminView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final ContactAdminController controller = Get.put(ContactAdminController());
+  _ContactAdminViewState createState() => _ContactAdminViewState();
+}
 
-    Widget header(BuildContext context) {
-      return Container(
-        height: 200,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: AppColors.deepOceanBlue,
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(20),
-            bottomRight: Radius.circular(20),
-          ),
+class _ContactAdminViewState extends State<ContactAdminView> {
+  late ContactAdminController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = Get.put(ContactAdminController());
+  }
+
+  Widget header() {
+    return Container(
+      height: 230,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: AppColors.deepOceanBlue,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
         ),
-        child: Stack(
-          children: [
-            Positioned(
-              top: 40,
-              left: 16,
-              child: GestureDetector(
-                onTap: () => Get.back(),
-                child: Row(
-                  children: [
-                    Icon(Icons.arrow_back, color: Colors.white, size: 28),
-                    SizedBox(width: 8),
-                    Text(
-                      "Kembali",
-                      style: GoogleFonts.poppins(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 70,
+            left: 15,
+            child: GestureDetector(
+              onTap: () => Get.back(),
+              child: Row(
+                children: [
+                  Icon(Icons.arrow_back, color: Colors.white),
+                  SizedBox(width: 8),
+                  Text(
+                    "Kembali",
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 100),
+              child: Text(
+                "Kontak Admin",
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget adminList() {
+    return Obx(() {
+      if (controller.isLoading.value) {
+        return Center(child: CircularProgressIndicator());
+      }
+
+      return ListView.builder(
+        shrinkWrap: true,
+        itemCount: controller.admins.length,
+        itemBuilder: (context, index) {
+          final admin = controller.admins[index];
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                
+              ),
+              child: Row(
+                children: [
+                    Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image(
+                      image: AssetImage('assets/images/coachSarah.jpg'),
+                      width: 70,
+                      height: 70,
                       ),
                     ),
-                  ],
-                ),
-              ),
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: Text(
-                  "Kontak Admin",
-                  style: GoogleFonts.poppins(
-                    color: Colors.white,
-                    fontSize: 36,
-                    fontWeight: FontWeight.w500,
+                    
                   ),
-                ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        admin.name,
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "${admin.email}\n${admin.noTelf}",
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                      
+                    ],
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          );
+        },
       );
-    }
+    });
+  }
 
-    Widget adminList(BuildContext context) {
-      return Obx(() {
-        if (controller.isLoading.value) {
-          return Center(child: CircularProgressIndicator());
-        }
-
-        return ListView.separated(
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          padding: EdgeInsets.all(16),
-          itemCount: controller.admins.length,
-          separatorBuilder: (_, __) => Divider(),
-          itemBuilder: (context, index) {
-            final admin = controller.admins[index];
-            return ListTile(
-              leading: Icon(Icons.person, color: AppColors.deepOceanBlue),
-              title: Text(
-                admin.name,
-                style: GoogleFonts.poppins(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              subtitle: Text(
-                "${admin.email}\n${admin.noTelf}",
-                style: GoogleFonts.poppins(
-                  fontSize: 14,
-                  color: Colors.grey[700],
-                ),
-              ),
-            );
-          },
-        );
-      });
-    }
-
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(children: [header(context), adminList(context)]),
-      ),
+      body: Column(
+        children: [
+          header(), adminList()
+          ]),
     );
   }
 }
