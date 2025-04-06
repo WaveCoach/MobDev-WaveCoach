223ec243c05d49cffa475798fe172467c4cba60f
cintarmynti <ramayanticinta@gmail.com>
2025-04-07 00:43:26 +0700

    merapihkan view

--- lib/app/modules/form_penilaian/views/form_penilaian_view.dart
+++ lib/app/modules/form_penilaian/views/form_penilaian_view.dart
@@ -3,119 +3,124 @@ import 'package:get/get.dart';
 import 'package:mob_dev_wave_coach/app/modules/form_penilaian/model/student_model.dart';
 import 'package:mob_dev_wave_coach/app/modules/schedule/controllers/schedule_controller.dart';
 import 'package:mob_dev_wave_coach/app/modules/schedule/model/schedule_response.dart';
-
 import '../controllers/form_penilaian_controller.dart';
 
 class FormPenilaianView extends GetView<FormPenilaianController> {
-  final formPenilaianController = Get.put(FormPenilaianController());
-  final scheduleController = Get.find<ScheduleController>();
-  final TextEditingController dateController = TextEditingController();
+  final FormPenilaianController penilaianController = Get.put(
+    FormPenilaianController(),
+  );
+  final ScheduleController scheduleController = Get.find<ScheduleController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Form Penilaian")),
+      appBar: AppBar(title: const Text("Form Penilaian")),
       body: SingleChildScrollView(
-        // agar bisa scroll saat konten tinggi
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Input tanggal
-              TextFormField(
-                controller: formPenilaianController.dateController,
-                decoration: InputDecoration(
-                  labelText: "Tanggal",
-                  border: OutlineInputBorder(),
-                  suffixIcon: Icon(Icons.calendar_today),
-                ),
-                readOnly: true,
-                onTap: () async {
-                  DateTime? pickedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2101),
-                  );
-                  if (pickedDate != null) {
-                    formPenilaianController.dateController.text =
-                        "${pickedDate.year}-${pickedDate.month.toString().padLeft(2, '0')}-${pickedDate.day.toString().padLeft(2, '0')}";
-
-                    // Fetch schedule setelah pilih tanggal
-                    await formPenilaianController.fetchSchedulesByDate(
-                      date: formPenilaianController.dateController.text,
-                    );
-                  }
-                },
-              ),
-              SizedBox(height: 16),
-
-              // Dropdown schedule
-              Obx(() {
-                if (scheduleController.isLoading.value) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                if (scheduleController.scheduleList.isEmpty) {
-                  return Center(child: Text("Tidak ada jadwal tersedia"));
-                }
-                return DropdownButtonFormField<Schedule>(
-                  isExpanded: true,
-                  value: formPenilaianController.selectedSchedule.value,
-                  items:
-                      scheduleController.scheduleList.map((schedule) {
-                        return DropdownMenuItem(
-                          value: schedule,
-                          child: Text(
-                            "${schedule.date} | ${schedule.startTime} - ${schedule.endTime}",
-                            style: TextStyle(fontSize: 14),
-                          ),
-                        );
-                      }).toList(),
-                  onChanged: (schedule) {
-                    formPenilaianController.selectedSchedule.value = schedule;
-                    formPenilaianController.fetchStudentBySchedule();
-                  },
-                  decoration: InputDecoration(
-                    labelText: "Pilih Jadwal",
-                    border: OutlineInputBorder(),
-                  ),
-                );
-              }),
-
-              SizedBox(height: 16),
-              Obx(() {
-                if (formPenilaianController.isLoading.value) {
-                  return Center(child: CircularProgressIndicator());
-                }
-
-                if (formPenilaianController.studentList.isEmpty) {
-                  return Center(child: Text("Tidak ada siswa tersedia"));
-                }
-
-                return DropdownButtonFormField<Student>(
-                  isExpanded: true,
-                  decoration: InputDecoration(
-                    labelText: "Pilih Siswa",
-                    border: OutlineInputBorder(),
-                  ),
-                  value: formPenilaianController.selectedStudent.value,
-                  items:
-                      formPenilaianController.studentList.map((student) {
-                        return DropdownMenuItem<Student>(
-                          value: student,
-                          child: Text(student.name),
-                        );
-                      }).toList(),
-                  onChanged: (Student? newValue) {
-                    formPenilaianController.selectedStudent.value = newValue;
-                  },
-                );
-              }),
-            ],
-          ),
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildDatePicker(context),
+            const SizedBox(height: 16),
+            _buildScheduleDropdown(),
+            const SizedBox(height: 16),
+            _buildStudentDropdown(),
+          ],
         ),
       ),
     );
+  }
+
+  Widget _buildDatePicker(BuildContext context) {
+    return TextFormField(
+      controller: penilaianController.dateController,
+      decoration: const InputDecoration(
+        labelText: "Tanggal",
+        border: OutlineInputBorder(),
+        suffixIcon: Icon(Icons.calendar_today),
+      ),
+      readOnly: true,
+      onTap: () async {
+        final pickedDate = await showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(2000),
+          lastDate: DateTime(2101),
+        );
+        if (pickedDate != null) {
+          final formattedDate =
+              "${pickedDate.year}-${pickedDate.month.toString().padLeft(2, '0')}-${pickedDate.day.toString().padLeft(2, '0')}";
+          penilaianController.dateController.text = formattedDate;
+
+          await penilaianController.fetchSchedulesByDate(date: formattedDate);
+        }
+      },
+    );
+  }
+
+  Widget _buildScheduleDropdown() {
+    return Obx(() {
+      if (scheduleController.isLoading.value) {
+        return const Center(child: CircularProgressIndicator());
+      }
+
+      if (scheduleController.scheduleList.isEmpty) {
+        return const Center(child: Text("Tidak ada jadwal tersedia"));
+      }
+
+      return DropdownButtonFormField<Schedule>(
+        isExpanded: true,
+        value: penilaianController.selectedSchedule.value,
+        decoration: const InputDecoration(
+          labelText: "Pilih Jadwal",
+          border: OutlineInputBorder(),
+        ),
+        items:
+            scheduleController.scheduleList.map((schedule) {
+              return DropdownMenuItem(
+                value: schedule,
+                child: Text(
+                  "${schedule.date} | ${schedule.startTime} - ${schedule.endTime}",
+                  style: const TextStyle(fontSize: 14),
+                ),
+              );
+            }).toList(),
+        onChanged: (selectedSchedule) {
+          penilaianController.selectedSchedule.value = selectedSchedule;
+          penilaianController.fetchStudentBySchedule();
+        },
+      );
+    });
+  }
+
+  Widget _buildStudentDropdown() {
+    return Obx(() {
+      if (penilaianController.isLoading.value) {
+        return const Center(child: CircularProgressIndicator());
+      }
+
+      if (penilaianController.studentList.isEmpty) {
+        return const Center(child: Text("Tidak ada siswa tersedia"));
+      }
+
+      return DropdownButtonFormField<Student>(
+        isExpanded: true,
+        value: penilaianController.selectedStudent.value,
+        decoration: const InputDecoration(
+          labelText: "Pilih Siswa",
+          border: OutlineInputBorder(),
+        ),
+        items:
+            penilaianController.studentList.map((student) {
+              return DropdownMenuItem(
+                value: student,
+                child: Text(student.name),
+              );
+            }).toList(),
+        onChanged: (selectedStudent) {
+          penilaianController.selectedStudent.value = selectedStudent;
+        },
+      );
+    });
   }
 }
